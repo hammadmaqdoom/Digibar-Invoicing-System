7903cf6eb4a3b166ac0d0a805a90ec2d5632cc9a
Lama Imam <li0607@st.habib.edu.pk>
2021-12-02 11:10:22 +0500

    FINALLY fixed transactions

--- crmapp/crm/urls.py
+++ crmapp/crm/urls.py
@@ -22,8 +22,8 @@ urlpatterns = [
     path("view_purchases",views.view_purchases, name='view_purchases'),
     path('delete_purchases/', views.delete_purchases, name='delete_purchases' ),
     path('update_purchases/', views.update_purchases, name='update_purchases' ),
-    path('view_transactions/', views.view_transactions, name='view_transactions' ),
-    path('delete_transactions/', views.update_transactions, name='delete_transactions' ),
+    path('view_transactions', views.view_transactions, name='view_transactions' ),
+    path('delete_transactions/', views.delete_transactions, name='delete_transactions' ),
     path('update_transactions/', views.update_transactions, name='update_transactions' ),
 
 
